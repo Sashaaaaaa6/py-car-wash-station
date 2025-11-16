0e5fd1e65beb5e13e61bfffaa6bf824813e5282f
Sashaaaaaa6 <o.bulavin14@gmail.com>
2025-11-16 17:53:02 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self, model: str, is_clean: bool = False) -> None:
+        self.model = model
+        self.is_clean = is_clean
+
+    def wash(self) -> None:
+        self.is_clean = True
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self) -> None:
+        self.was_cleaned = 0
+
+    def wash_car(self, car: Car) -> None:
+        car.wash()
+        self.was_cleaned += 1
